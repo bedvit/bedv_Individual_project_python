30f08ed4c20b9927fa0fbe50507d6969181ad657
Vitaly Bedny <bedvit@mail.ru>
2023-01-09 22:27:43 +0300

    +

--- bedv_Individual_project_python/bedv_Individual_project_python.py
+++ bedv_Individual_project_python/bedv_Individual_project_python.py
@@ -63,11 +63,13 @@ def main():
 
 
 	records4.sort()
-	for i in range(0, len(records4)-1):
+	i=0
+	while i<len(records4)-1:
 		if records4[i][0]==records4[i+1][0]:
 			records.append(records4[i])
-
-
+			records.append(records4[i+1])
+			i+=2
+		i+=1
 	df = pd.DataFrame(records)
 	df.to_excel( 'ResultHard.xlsx', sheet_name='hard', header=False, index=False )
 	cursor.executemany( """INSERT INTO de11an.bedv_med_results(telephone, patient_name,analysis_name,conclusion) VALUES (%s,%s,%s,%s)""", df.values.tolist())
@@ -81,6 +83,91 @@ if __name__ == '__main__':
 
 
 
+
+#import psycopg2
+#import pandas as pd
+
+#def main():	
+#	conn = psycopg2.connect(database = "edu",
+#							host =     "de-edu-db.chronosavant.ru",
+#							user =     "de11an",
+#							password = "peregrintook",
+#							port =     "5432")
+	
+#	conn.autocommit = False
+#	cursor = conn.cursor()
+
+#	cursor.execute("CREATE TABLE IF NOT EXISTS de11an.bedv_med_results (telephone text, patient_name text, analysis_name text, conclusion text )" )
+#	cursor.execute("TRUNCATE TABLE de11an.bedv_med_results" )
+#	conn.commit()
+
+#	cursor.execute( "SELECT * FROM de.med_an_name" )
+#	records2 = cursor.fetchall()
+
+#	cursor.execute( "SELECT * FROM de.med_name" )
+#	records3 = cursor.fetchall()
+
+#	# Формирование DataFrame вариант: easy
+#	df1 = pd.read_excel( r'medicine.xlsx', sheet_name='easy', header=0, index_col=None )
+#	records1=df1.values.tolist()
+#	records = []
+#	for row1 in records1:
+#		for row2 in records2:
+#			if row1[1]==row2[0] and (row1[2]<row2[3] or row1[2]>row2[4]):
+#				for row3 in records3:
+#					if row1[0]==row3[0]:
+#						records.append([row3[2],row3[1],row2[1],'Понижен' if row1[2]<row2[3] else 'Повышен'])
+
+
+#	df = pd.DataFrame(records)
+#	df.to_excel( 'ResultEasy.xlsx', sheet_name='easy', header=False, index=False )
+#	cursor.executemany( """INSERT INTO de11an.bedv_med_results(telephone, patient_name,analysis_name,conclusion) VALUES (%s,%s,%s,%s)""", df.values.tolist())
+#	conn.commit()
+
+#	# Формирование DataFrame вариант: hard
+#	cursor.execute("TRUNCATE TABLE de11an.bedv_med_results" )
+#	records.clear()
+#	records4 = []
+#	df1 = pd.read_excel( r'medicine.xlsx', sheet_name='hard', header=0, index_col=None )
+#	records1=df1.values.tolist()
+
+#	for row1 in records1:
+
+#		if type(row1[2])==str and row1[2].replace('.', '').isdigit():
+#			row1[2]=float(row1[2])
+
+#		if type(row1[2])==str:
+#			if row1[2]=='+' or 'Пол' in row1[2]:
+#				row1[2]=1
+
+#		for row2 in records2:
+#			if ((type(row1[2])==float or type(row1[2])==int) and row1[1]==row2[0] and (row1[2]==1 or row1[2]<row2[3] or row1[2]>row2[4])):
+#				for row3 in records3:
+#					if row1[0]==row3[0]:
+#						records4.append([row3[2],row3[1],row2[1],'Положительный' if row1[2]==1 else ('Понижен' if row1[2]<row2[3] else 'Повышен')])
+
+
+#	records4.sort()
+#	i=0
+#	while i<len(records4)-1:
+#		if records4[i][0]==records4[i+1][0]:
+#			records.append(records4[i])
+#			records.append(records4[i+1])
+#			i+=2
+#		i+=1
+#	df = pd.DataFrame(records)
+#	df.to_excel( 'ResultHard.xlsx', sheet_name='hard', header=False, index=False )
+#	cursor.executemany( """INSERT INTO de11an.bedv_med_results(telephone, patient_name,analysis_name,conclusion) VALUES (%s,%s,%s,%s)""", df.values.tolist())
+#	conn.commit()
+
+#	cursor.close()
+#	conn.close()
+
+#if __name__ == '__main__':
+#    main()
+
+
+#тренировочный модуль
 	#def main():	
 	#conn = psycopg2.connect(database = "edu",
 	#						host =     "de-edu-db.chronosavant.ru",
